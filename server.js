6642490726e4a87ca9e73aa1602c015ee661ca8f
quickcashsavvy-archTaiwo <quickcashsavvy@gmail.com>
2026-03-31 14:07:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ await downloadFile(audio_url, audioPath);
 
     await new Promise((resolve, reject) => {
       exec(
-        `ffmpeg -y -stream_loop -1 -i "${videoPath}" -i "${audioPath}" -t ${duration} -shortest -c:v libx264 -c:a aac "${outputPath}"`,
+  `ffmpeg -y -i "${videoPath}" -i "${audioPath}" -map 0:v:0 -map 1:a:0 -t ${duration} -c:v libx264 -preset veryfast -pix_fmt yuv420p -c:a aac -shortest "${outputPath}"`,
         (err, stdout, stderr) => {
           if (err) {
             console.error("FFmpeg error:", stderr);
