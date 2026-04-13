2d2bf226cd1854c9d8bf79fe16dc80d7e892a7af
quickcashsavvy-archTaiwo <quickcashsavvy@gmail.com>
2026-04-13 11:46:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -200,8 +200,6 @@ const filter = subtitleFilter
   ? `[0:v]setpts=${stretchFactor}*PTS,${subtitleFilter}[v]`
   : `[0:v]setpts=${stretchFactor}*PTS[v]`;
 
-const originalDuration = await getVideoDuration(videoPath);
-
 // ✅ STRETCH FACTOR (NO LIMIT ❌)
 const stretchFactor = duration / originalDuration;
 
